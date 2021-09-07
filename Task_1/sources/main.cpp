a745dddedfd2b9ee237915d7c7a458d53b3d0add
qDerezzed <prohoroff.anton2013@yandex.ru>
2021-09-08 02:47:20 +0300

    потоки не создаются заново на каждую итерацию цикла REPEATS_COUNT

--- Task_1/sources/main.cpp
+++ Task_1/sources/main.cpp
@@ -4,14 +4,96 @@
 #include <unistd.h>
 #include <random>
 #include <string>
+#include <queue>
 
-#define NUM_READERS 10
-#define NUM_WRITERS 20
+#define NUM_READERS 5
+#define NUM_WRITERS 10
 
 #define READER_TIME 1
 #define WRITER_TIME 2
 
 #define REPEATS_COUNT 3
+
+class Pool {
+public:
+    Pool() = delete;
+
+    ~Pool() {
+        cancelled = true;
+        pthread_mutex_lock(&mutex);
+        pthread_cond_broadcast(&cond);
+        pthread_mutex_unlock(&mutex);
+        for (size_t i = 0; i < amountThreads; i++) {
+            pthread_join(threads[i], nullptr);
+        }
+    }
+
+    Pool(void *(*inputFunction)(void *), size_t inputAmountThreads) :
+            pFunction(inputFunction), amountThreads(inputAmountThreads) {
+        threads.resize(amountThreads);
+        pthread_mutex_init(&mutex, nullptr);
+        pthread_cond_init(&cond, nullptr);
+        for (size_t i = 0; i < amountThreads; i++) {
+            pthread_create(&threads[i], nullptr, &Pool::thread, this);
+        }
+    }
+
+    void addToPool(void *arg) {
+        pthread_mutex_lock(&mutex);
+        queue.push(arg);
+        remaining++;
+        pthread_cond_signal(&cond);
+        pthread_mutex_unlock(&mutex);
+    }
+
+    void wait() {
+        pthread_mutex_lock(&mutex);
+        while (!cancelled && remaining) {
+            pthread_cond_wait(&cond, &mutex);
+        }
+        pthread_mutex_unlock(&mutex);
+    }
+
+private:
+    bool cancelled = false;
+
+    void *(*pFunction)(void *);
+
+    size_t remaining = 0;
+    size_t amountThreads;
+    std::queue<void *> queue;
+    pthread_mutex_t mutex{};
+    pthread_cond_t cond{};
+    std::vector<pthread_t> threads;
+
+    static void *thread(void *inputPool) {
+        auto pool = static_cast<Pool *>(inputPool);
+        while (!pool->cancelled) {
+            pthread_mutex_lock(&pool->mutex);
+            while (!pool->cancelled && pool->queue.empty()) {
+                pthread_cond_wait(&pool->cond, &pool->mutex);
+            }
+            if (pool->cancelled) {
+                pthread_mutex_unlock(&pool->mutex);
+                return nullptr;
+            }
+
+            auto temp = pool->queue.front();
+            pool->queue.pop();
+
+            pthread_mutex_unlock(&pool->mutex);
+
+            pool->pFunction(temp);
+
+            pthread_mutex_lock(&pool->mutex);
+            pool->remaining--;
+            pthread_cond_broadcast(&pool->cond);
+            pthread_mutex_unlock(&pool->mutex);
+        }
+
+        return nullptr;
+    }
+};
 
 pthread_mutex_t printMutex;
 
@@ -29,8 +111,6 @@ pthread_mutex_t mutexW;
 
 pthread_cond_t readingEnd;
 pthread_cond_t writingEnd;
-
-pthread_barrier_t barrier;
 
 void *read(void *str) {
     auto pthreadId = pthread_self();
@@ -58,7 +138,6 @@ void *read(void *str) {
     }
     pthread_mutex_unlock(&mutexR);
 
-    pthread_barrier_wait(&barrier);
 
     return nullptr;
 }
@@ -89,58 +168,43 @@ void *write(void *args) {
     }
     pthread_mutex_unlock(&mutexW);
 
-    pthread_barrier_wait(&barrier);
 
     return nullptr;
 }
 
 int main() {
-    std::vector<pthread_t> threads(NUM_READERS + NUM_WRITERS);
-
     pthread_mutex_init(&mutexR, nullptr);
     pthread_mutex_init(&mutexW, nullptr);
 
     pthread_cond_init(&writingEnd, nullptr);
     pthread_cond_init(&readingEnd, nullptr);
 
-    pthread_barrier_init(&barrier, nullptr, NUM_READERS + NUM_WRITERS + 1);
-
     std::random_device rd;
     std::mt19937 mersenne(rd());
     auto *str = new std::string;
+    Pool writersPool(write, NUM_WRITERS);
+    Pool readersPool(read, NUM_READERS);
     for (size_t repeat = 1; repeat <= REPEATS_COUNT; ++repeat) {
         std::cout << "Iteration №" << repeat << std::endl;
         size_t numWriters = 0, numReaders = 0;
         *str = "@";
         while (numWriters < NUM_WRITERS || numReaders < NUM_READERS) {
             if (mersenne() % 2 == 0 && numWriters < NUM_WRITERS) {
-                if (pthread_create(&threads[numWriters + numReaders], nullptr, write, static_cast<void *>(str))) {
-                    std::cout << "Error! Thread cant be created!";
-                    return -1;
-                }
+                writersPool.addToPool(static_cast<void *>(str));
                 ++numWriters;
             } else if (numReaders < NUM_READERS) {
-                if (pthread_create(&threads[numWriters + numReaders], nullptr, read, static_cast<void *>(str))) {
-                    std::cout << "Error! Thread cant be created!";
-                    return -1;
-                }
+                readersPool.addToPool(static_cast<void *>(str));
                 ++numReaders;
             }
         }
-
-        pthread_barrier_wait(&barrier);
-        for (size_t thread : threads) {
-            pthread_join(thread, nullptr);
-        }
-
+        writersPool.wait();
+        readersPool.wait();
         std::cout << std::endl;
     }
     delete str;
-    pthread_barrier_destroy(&barrier);
     pthread_cond_destroy(&writingEnd);
     pthread_cond_destroy(&readingEnd);
     pthread_mutex_destroy(&mutexR);
     pthread_mutex_destroy(&mutexW);
-
     return 0;
 }
